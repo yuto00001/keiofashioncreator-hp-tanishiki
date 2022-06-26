f43085aef8c75efc669330762e8c7f38d3d39140
YUTO TANISHIKI <yutotanishiki@TANISHIKI.local>
2022-06-26 17:28:59 +0900

    IntersectionObserverのスタイルを調整

--- public/js/IntersectionObserver.js
+++ public/js/IntersectionObserver.js
@@ -2,12 +2,23 @@
 
 {
   const targets = document.querySelectorAll('.animation_intersection');
+  // targetが交差するタイミングを指定した変数。
+  const options = {
+    threshold: 1,
+    rootMargin: '0px 0px -80px',
+  };
+
+  // IntersectionObserver クラスはあらかじめブラウザが用意してくれているクラス
+  // オブジェクトと表示領域の 重なりを監視するために必要なデータや処理をまとめたクラス
+  // 条件に当てはまれば callback に指定した処理を実行
+  const observer = new IntersectionObserver(callback, options);
+
+
 
   // こちらの処理が実行されるタイミングですが、監視が始まったときと、デフォルトだと target が root に 0% 交差したときに実行されます。
   // この entries は複数の要素をもつ配列で、監視開始時点ではすべての target が、そして target が交差したときは、交差した target だけが要素として入ってくる
   function callback(entries, obs) {
     console.log(entries[0]);
-
   // では、交差した要素を entry として、次の処理をしてねと書いていきましょう。
   // isIntersectingはターゲット要素が交差している場合に True になります。つまり下記の場合、「true だったら appear クラスを付け、交差が終わるときは早期returnをしてアニメーションをしないようにする」となる。
     entries.forEach(entry => {
@@ -21,16 +32,6 @@
     });
   }
 
-  // targetが交差するタイミングを指定した変数。
-  const options = {
-    threshold: 1,
-    rootMargin: '0px 0px -100px',
-  };
-
-  // IntersectionObserver クラスはあらかじめブラウザが用意してくれているクラス
-  // オブジェクトと表示領域の 重なりを監視するために必要なデータや処理をまとめたクラス
-  // 条件に当てはまれば callback に指定した処理を実行
-  const observer = new IntersectionObserver(callback, options);
 
   // observerによってtarget(img)を監視対象とする
   targets.forEach(target => {
